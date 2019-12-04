77ac8ea28ecf9e8a6032640d4b7880763a10444a
Yusuf İşleyen <isleyenyusuf@gmail.com>
2019-12-04 22:53:14 +0300

    site information completed

---
+++ CMS.Data/Db/Func.cs
@@ -0,0 +1,72 @@
+﻿using Dapper.FastCrud;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Db
+{
+    public class Func
+    {
+        #region about-us
+
+        #endregion
+
+        #region admin
+
+        #endregion
+
+        #region blog
+
+        #endregion
+
+        #region category
+
+        #endregion
+
+        #region contact
+
+        #endregion
+
+        #region our-services
+
+        #endregion
+
+        #region site-infos
+
+        // Get site info
+        // title, keywords, description, logoUrl, degree
+        public static SiteInfos GetSiteInfos()
+        {
+            var connection = Repository.GetOpenConnection();
+            {
+                var siteInfos = connection.Get(new SiteInfos { id = 1 });
+
+                return siteInfos;
+            }
+        }
+
+        // Insert site info
+        public static void SaveOrUpdateSiteInfos(SiteInfos siteInfos, int siteinfoid)
+        {
+            if (siteinfoid == 0) // save if user number is 0 
+            {
+                var connection = Repository.GetOpenConnection();
+                {
+                    connection.Insert(siteInfos);
+                }
+            }
+            else // update if user number is not 0
+            {
+                var connection = Repository.GetOpenConnection();
+                {
+                    connection.Update(siteInfos);
+                }
+            }
+        }
+
+
+        #endregion
+    }
+}
--- CMS.Web/Controllers/SiteInfoController.cs
+++ CMS.Web/Controllers/SiteInfoController.cs
@@ -1,17 +1,66 @@
-﻿using System;
+﻿using CMS.Data;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace CMS.Web.Controllers
 {
     public class SiteInfoController : Controller
     {
+        string uploadPath = ConfigurationManager.AppSettings["UploadPath"];
+        string logoWidth = ConfigurationManager.AppSettings["LogoWidth"];
+        string logoHeight = ConfigurationManager.AppSettings["LogoHeight"];
+
         // GET: SiteInfo
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            SiteInfos siteInfos = Data.Db.Func.GetSiteInfos(); // get site infos
+
+            if (siteInfos == null)
+            {
+                siteInfos = new SiteInfos(); // new site infos
+
+                return View(siteInfos); // return site infos
+            }
+            else
+            {
+                return View(siteInfos); // return site infos
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(FormCollection form, HttpPostedFileBase logoUrl)
+        {
+            SiteInfos siteInfos = new SiteInfos();
+
+            TryUpdateModel(siteInfos, form);
+
+            int id = Convert.ToInt32(form["id"]);
+
+            if (logoUrl != null)
+            {
+                string logoName = "site-logo.png"; // new file name
+
+                if (System.IO.File.Exists(uploadPath + logoName)) // file control
+                {
+                    System.IO.File.Delete(uploadPath + logoName);
+                }
+
+                WebImage webImage = new WebImage(logoUrl.InputStream);
+
+                webImage.Save(uploadPath + logoName);
+
+                siteInfos.logoUrl = logoName;
+            }
+
+            Data.Db.Func.SaveOrUpdateSiteInfos(siteInfos, id);
+
+            return RedirectToAction("Index", "SiteInfo");
         }
     }
 }
