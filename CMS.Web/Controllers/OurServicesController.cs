ff27546d2f3bfac4f81e2bace285336370bdabc4
Yusuf İşleyen <isleyenyusuf@gmail.com>
2019-12-06 17:08:32 +0300

    our services started

---
+++ CMS.Web/Controllers/OurServicesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Web.Controllers
+{
+    public class OurServicesController : Controller
+    {
+        [HttpGet]
+        // GET: OurServices
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- CMS.Web/Controllers/SiteInfosController.cs
+++ CMS.Web/Controllers/SiteInfosController.cs
@@ -1,21 +1,19 @@
 ﻿using CMS.Data;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace CMS.Web.Controllers
 {
-    public class SiteInfoController : Controller
+    public class SiteInfosController : Controller
     {
         string uploadPath = ConfigurationManager.AppSettings["UploadPath"];
         string logoWidth = ConfigurationManager.AppSettings["LogoWidth"];
         string logoHeight = ConfigurationManager.AppSettings["LogoHeight"];
 
-        // GET: SiteInfo
+        // GET: SiteInfos
         [HttpGet]
         public ActionResult Index()
         {
@@ -61,7 +59,7 @@ namespace CMS.Web.Controllers
 
             Data.Db.Func.SaveOrUpdateSiteInfos(siteInfos, id);
 
-            return RedirectToAction("Index", "SiteInfo");
+            return RedirectToAction("Index", "SiteInfos");
         }
     }
 }
